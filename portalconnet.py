b234f0b550e0aeee1e0cf3b835ab8691e55b1910
huangsinchu <huangsinchu@gmail.com>
2015-09-22 11:00:46 +0800

    Simplify

--- portalconnet.py
+++ portalconnet.py
@@ -1,6 +1,7 @@
 import urllib,urllib2
 import json
 import random,hashlib
+import time
 
 #Change your id and password here.
 username = '12125xxxx'
@@ -10,9 +11,6 @@ url = 'http://p.nju.edu.cn/portal_io/'
 url_logout = url+'logout'
 url_login = url+'login'
 
-def fromCharCode(a, *b):
-    return unichr(a%65536) + ''.join([unichr(i%65536) for i in b])
-
 def GetChallenge():
 	url_getchallenge = url+'getchallenge'
 	challenge_data = urllib2.urlopen(url_getchallenge).read()
@@ -21,19 +19,19 @@ def GetChallenge():
 
 def ChapPassword(password,challenge):
 	id = random.randint(0,255)
-	print 'id='+str(id)
+	#print 'id='+str(id)
 	strs = ''
-	strs += fromCharCode(id)
+	strs += unichr(id)
 	strs += password
 	i = 0
 	length = len(challenge)
 	while i < length:
 		hex_tmp = challenge[i:i+2]
 		dec_tmp = int(hex_tmp,16)
-		strs += fromCharCode(dec_tmp)
+		strs += unichr(dec_tmp)
 		i += 2
 	hash = hashlib.md5(strs.encode('ISO-8859-1')).hexdigest()
-	print hash
+	#print hash
 	result = ''
 	if id<16:
 		result = '0'+hex(id)[2:]+hash
@@ -51,6 +49,6 @@ data['challenge'] = challenge
 
 post_data = urllib.urlencode(data)
 
-#req_logout = urllib2.urlopen(url_logout)
-#time.sleep(3)
+req_logout = urllib2.urlopen(url_logout)
+time.sleep(2)
 req_login = urllib2.urlopen(url_login, post_data)
